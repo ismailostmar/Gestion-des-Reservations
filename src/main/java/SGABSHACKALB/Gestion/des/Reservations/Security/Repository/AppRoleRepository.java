5fb3e58042380e45c438d352fa85f8d910a92a45
Flex-Office SFE <TMAR_ISMAIL@emsi-edu.ma>
2022-06-16 15:26:31 +0100

    @Repository annotation.

--- src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/Repository/AppRoleRepository.java
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/Repository/AppRoleRepository.java
@@ -2,7 +2,9 @@ package SGABSHACKALB.Gestion.des.Reservations.Security.Repository;
 
 import SGABSHACKALB.Gestion.des.Reservations.Security.Entities.AppRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
     AppRole findByRoleName( String roleName);
 }
--- src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/Repository/AppUserRepository.java
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/Repository/AppUserRepository.java
@@ -2,7 +2,9 @@ package SGABSHACKALB.Gestion.des.Reservations.Security.Repository;
 
 import SGABSHACKALB.Gestion.des.Reservations.Security.Entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser,Long> {
     AppUser findByUsername(String username);
 }
