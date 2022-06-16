ed7eab5182f6e6ba7064139da165caf1c6bbc0f5
Flex-Office SFE <TMAR_ISMAIL@emsi-edu.ma>
2022-06-16 15:27:32 +0100

    Enable Security Configuration and Encoding Password

--- src/main/java/SGABSHACKALB/Gestion/des/Reservations/GestionDesReservationsApplication.java
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/GestionDesReservationsApplication.java
@@ -1,27 +1,37 @@
 package SGABSHACKALB.Gestion.des.Reservations;
 
+import SGABSHACKALB.Gestion.des.Reservations.Security.Entities.AppRole;
+import SGABSHACKALB.Gestion.des.Reservations.Security.Entities.AppUser;
+import SGABSHACKALB.Gestion.des.Reservations.Security.Services.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableSwagger2
+// PrePost Enabled & SecureEnabled
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class GestionDesReservationsApplication {
-
-	/*public static final String dateFormat = "yyyy-MM-dd";
-	public static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";*/
-
     public static void main(String[] args) {
         SpringApplication.run(GestionDesReservationsApplication.class, args);
     }
-
-	/*@Bean
-	public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer(){
-		return builder -> {
-			builder.simpleDateFormat(dateFormat);
-			builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
-			builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
-		};
-	}*/
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    CommandLineRunner start(AccountService accountService){
+        return args -> {
+            accountService.addNewRole(new AppRole(null,"ADMIN"));
+            accountService.addNewUser(new AppUser(null,"USER1","1234",new ArrayList<>()));
+            accountService.addRoleToUser("USER1","ADMIN");
+        };
+    }
 }
