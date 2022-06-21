54e9c148df36e07bb1139dab73c8bf5f0f2d19ba
Flex-Office SFE <TMAR_ISMAIL@emsi-edu.ma>
2022-06-21 15:41:05 +0100

    Filtres : JwtAuthorizationFilter.

---
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/Filters/JwtAuthorizationFilter.java
@@ -0,0 +1,68 @@
+package SGABSHACKALB.Gestion.des.Reservations.Security.Filters;
+
+import SGABSHACKALB.Gestion.des.Reservations.Security.JWTUtile;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().equals("/refreshToken")){
+            filterChain.doFilter(request,response);
+        }
+        else {
+            String authorizationToken = request.getHeader(JWTUtile.AUTH_HEADER);
+            if(authorizationToken != null && authorizationToken.startsWith(JWTUtile.PREFIX)){
+                try {
+                    // Test of Request & Verification
+                    String jwt = authorizationToken.substring(7);
+                    Algorithm algorithm = Algorithm.HMAC256(JWTUtile.SECRET);
+
+                    // Verification
+
+                    JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = jwtVerifier.verify(jwt);
+
+                    String username = decodedJWT.getSubject();
+                    String[] roles = decodedJWT.getClaim(JWTUtile.CLAIM_NAME).asArray(String.class);
+                    Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+                    for (String r:roles){
+                        authorities.add(new SimpleGrantedAuthority(r));
+                    }
+
+                    // The User here can be Authenticated
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(username,null,authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                    // Before Authentication
+                    filterChain.doFilter(request,response);
+                }
+                catch (Exception e){
+                    response.setHeader("Error-Message", e.getMessage());
+                    // Si le Token est expiré , alors l'utilisateur n'est pas alloué d'avoir l'access
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                }
+            }
+            else {
+                filterChain.doFilter(request,response);
+            }
+        }
+    }
+}
