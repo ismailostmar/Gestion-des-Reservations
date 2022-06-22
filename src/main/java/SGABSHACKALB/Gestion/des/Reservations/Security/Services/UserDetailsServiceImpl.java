19b73aaccc063cc5216947c8245035d0bf88dab4
Flex-Office SFE <TMAR_ISMAIL@emsi-edu.ma>
2022-06-22 11:29:53 +0100

    This is mean the Stateful Authentication with the Session ID of Each User and his Authority.

---
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/Services/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package SGABSHACKALB.Gestion.des.Reservations.Security.Services;
+
+import SGABSHACKALB.Gestion.des.Reservations.Security.Entities.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AccountService accountService;
+
+    public UserDetailsServiceImpl(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // StateFul Authentication With Session ID
+    // User Details service is for security using the User Spring
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = accountService.loadUserByUsername(username);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        appUser.getAppRoles().forEach(r -> {
+            authorities.add(new SimpleGrantedAuthority(r.getRoleName()));
+        });
+        return new User(appUser.getUsername(), appUser.getPassword(), authorities);
+    }
+}
