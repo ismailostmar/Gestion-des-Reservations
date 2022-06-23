369eb312a23e61444d78e6561c0c8f19dee0539d
Flex-Office SFE <TMAR_ISMAIL@emsi-edu.ma>
2022-06-23 15:23:56 +0100

    Add More Users, Roles and AddRole to a User

--- src/main/java/SGABSHACKALB/Gestion/des/Reservations/GestionDesReservationsApplication.java
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/GestionDesReservationsApplication.java
@@ -31,9 +31,18 @@ public class GestionDesReservationsApplication {
     @Bean
     CommandLineRunner start(AccountService accountService) {
         return args -> {
-            accountService.addNewRole(new AppRole(null, "ADMIN"));
-            accountService.addNewUser(new AppUser(null, "USER", "1234", new ArrayList<>()));
-            accountService.addRoleToUser("USER", "ADMIN");
+            accountService.addNewRole(new AppRole(null,"ADMIN"));
+            accountService.addNewRole(new AppRole(null, "USER"));
+
+            accountService.addNewUser(new AppUser(null, "user1", "1234", new ArrayList<>()));
+            accountService.addNewUser(new AppUser(null, "admin", "1234", new ArrayList<>()));
+            accountService.addNewUser(new AppUser(null, "user2", "1234", new ArrayList<>()));
+            accountService.addNewUser(new AppUser(null, "user3", "1234", new ArrayList<>()));
+
+            accountService.addRoleToUser("user1", "USER");
+            accountService.addRoleToUser("admin","ADMIN");
+            accountService.addRoleToUser("user2","USER");
+            accountService.addRoleToUser("user3","USER");
         };
     }
 }
