342de68389ac26b7ce7b1e9c876cb5fc62235743
Flex-Office SFE <TMAR_ISMAIL@emsi-edu.ma>
2022-06-24 10:59:37 +0100

    Customizing these parameters to test how much the Access and the Refresh Token well be expired

--- src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/JWTUtile.java
+++ src/main/java/SGABSHACKALB/Gestion/des/Reservations/Security/JWTUtile.java
@@ -1,14 +1,14 @@
 package SGABSHACKALB.Gestion.des.Reservations.Security;
 
 public class JWTUtile {
-    public static final String SECRET="SecretHACKLABHACK";
-    public static final String AUTH_HEADER="Authorization";
-    public static final long EXPIRE_ACCESS_TOKEN=2*6*1000;
-    public static final long EXPIRE_REFRESH_TOKEN=15*6*1000;
-    public static final String PREFIX="Bearer ";
-    public static final String REFRESH_TOKEN_REQUIRED="REFRESH TOKEN REQUIRED";
-    public static  final String CONTENT_TYPE="application/json";
-    public static  final String CLAIM_NAME="roles";
-    public static final String ACCESS_TKN="Access_Token";
-    public static final String REFRESH_TKN="Refresh_Token";
+    public static String SECRET="SecretHACKLABHACK";
+    public static String AUTH_HEADER="Authorization";
+    public static long EXPIRE_ACCESS_TOKEN=20*6*1000;
+    public static long EXPIRE_REFRESH_TOKEN=300*6*1000;
+    public static String PREFIX="Bearer ";
+    public static String REFRESH_TOKEN_REQUIRED="REFRESH TOKEN REQUIRED";
+    public static String CONTENT_TYPE="application/json";
+    public static String CLAIM_NAME="roles";
+    public static String ACCESS_TKN="Access_Token";
+    public static String REFRESH_TKN="Refresh_Token";
 }
